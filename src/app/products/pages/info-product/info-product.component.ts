046a5c6063bc62912170b88f126fc1ab52382681
Ronaldo Torres <rtorres66@misena.edu.co>
2023-03-07 21:30:34 -0500

    Feat: Se agrego pagina de información del producto y su respectivo servicio

--- src/app/products/pages/info-product/info-product.component.ts
+++ src/app/products/pages/info-product/info-product.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+
+import { ProductsService } from '../../../services/products.service';
+import { Product } from '../../models/producto.interface';
 
 @Component({
   selector: 'app-info-product',
@@ -8,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoProductComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+
+  constructor(
+    private productService: ProductsService,
+    private activateRoute: ActivatedRoute,
+    private route: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.productService.getProductById(id))
+      )
+      .subscribe( product => this.product = product );
+  }
+
+  backAllProducts() {
+    this.route.navigateByUrl('/')
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Products } from '../products/models/productos.interface';
 import { Observable } from 'rxjs';
+
+import { Products } from '../products/models/productos.interface';
+import { Product } from '../products/models/producto.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +18,10 @@ export class ProductsService {
 
   getAllProducts(limit = '10', sort = 'desc', category?: string): Observable<Products[]> {
     return this.http.get<Products[]>(`${ environment.url }/products${category ? '/category/' + category : ''}?sort=${sort}&limit=${limit}`);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${ environment.url }/products/${ id }`);
   }
 
 }
